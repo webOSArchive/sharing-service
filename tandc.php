b531d00e168d20916d947d54a2570247894cf903
CodePoet80 <jonathan.m.wise@gmail.com>
2021-05-23 08:56:37 -0400

    update tandc

--- tandc.php
+++ tandc.php
@@ -7,4 +7,5 @@ $config = include('config.php');
     <li>This server does not allow the sharing of pornography, legal or otherwise. The host of this service will comply with any legal requests for logs or data. Your IP address will likely be visible in the logs, and the host provides no protection or indemnification.</li>
     <li>The host of this service reserves the right to deny service to anyone whose use of the service is not in good faith or has the appearance of being unreasonable, damaging, or irresponsible.</li>
     <li>If your share space goes unused for more than 30 days, it may be automatically purged.</li>
+    <li>Lost passwords cannot be recovered or reset. Please record your credentials in a secure location.</li>
 </ul>
