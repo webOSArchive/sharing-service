93396be912a88cbaee8fe198bed986c84834c47c
CodePoet80 <jonathan.m.wise@gmail.com>
2021-05-27 11:41:27 -0400

    update example

--- config-example.php
+++ config-example.php
@@ -3,11 +3,12 @@ return array(
 	'clientids' => array (
         'test',
     ),
-    'createkey' => 'creator',
+    'createkey' => 'creator',   //use a key if you want to limit new accounts
     'readonlykey' => "readonlytest",
     'shorturl' => 'http://short.link/',
     'maxsharelength' => 20,
     'maximagesize' => 3072000,
     'maxtextlength' => 5000,
+    'admincontact' => 'your@email'  //will be encoded in the agreement webpage
 );
 ?>
